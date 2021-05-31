43f134b0745c6832cc8de32922d105f8d9d9fb1e
zcvaters <zachvaters@gmail.com>
2021-05-31 19:28:12 -0230

    Fix localhost network error

--- views/src/context/FormContextProvider.js
+++ views/src/context/FormContextProvider.js
@@ -85,7 +85,7 @@ export const FormContextProvider = (props) => {
     }
 
     axios
-      .post(`/stats/${graphType}`, body)
+      .post(`https://localhost:3001/stats/${graphType}`, body)
       .then((resp) => {
         if (resp?.data?.err) {
           alert(JSON.stringify(resp?.data?.err));
