1b073767f11cda23e010189442aa0836eabb3096
zcvaters <zachvaters@gmail.com>
2021-05-31 20:42:11 -0230

    add proxy

--- views/src/context/FormContextProvider.js
+++ views/src/context/FormContextProvider.js
@@ -85,7 +85,7 @@ export const FormContextProvider = (props) => {
     }
 
     axios
-      .post(`https://localhost:${process.env.PORT || 3000}/stats/${graphType}`, body)
+      .post(`/stats/${graphType}`, body)
       .then((resp) => {
         if (resp?.data?.err) {
           alert(JSON.stringify(resp?.data?.err));
